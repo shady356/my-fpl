8bd7ccb964fb9fd76ccc6709d6f40362d863f4d3
shady356 <heenriko@gmail.com>
2020-03-08 22:35:29 +0100

    base input

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,17 +1,36 @@
 <template>
   <div class="login-container">
-    <h3>Enter FPL team number</h3>
+    <h2 class="login-title">Enter FPL Team ID</h2>
+    <base-input
+      class="team-id-input"
+      placeholder="Team-ID"
+    />
   </div>
 </template>
 
 <script>
+import BaseInput from '@/components/base/BaseInput.vue'
 export default {
-  name: 'Login'
+  name: 'Login',
+  components: {
+    'base-input': BaseInput
+  }
 }
 </script>
 
 <style lang="scss" scoped>
   .login-container {
     margin: $s;
+    display: flex;
+    align-items: center;
+    flex-direction: column;
+
+    .login-title {
+      margin: $xl 0 $s;
+    }
+
+    .team-id-input {
+      margin: $m 0;
+    }
   }
 </style>
