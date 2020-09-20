f3798de6aa102a689f2bf6d726bfb426d4dc4a5f
shady356 <heenriko@gmail.com>
2020-09-21 01:28:01 +0200

    feat: display team list in stats

---
+++ src/components/stats/StatsTeamListItem.vue
@@ -0,0 +1,48 @@
+<template>
+  <div class="team-container">
+    <img
+      :src="teamBadge" 
+      alt="team-badge"
+      class="team-badge"
+    >
+    <h4>{{team.name}}</h4>
+  <!--   <div v-if="team.name === 'Arsenal'">
+      <pre>{{team}}</pre>
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatsTeamListItem',
+  props: {
+    team: {
+      type: Object,
+      required: true
+    }
+  },
+  computed: {
+    teamBadge () {
+      return `https://fantasy.premierleague.com/dist/img/badges/badge_${this.team.code}_80.png#/`
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .team-container {
+    background: #1a1a1a;
+    display: flex;
+    align-items: center;
+    margin: $m 0;
+    padding: $m;
+    border-radius: $s;
+    border: 1px solid #333;
+
+    .team-badge {
+      margin-right: $m;
+      width: $l;
+      height: auto;
+    }
+  }
+</style>
--- src/views/Stats.vue
+++ src/views/Stats.vue
@@ -1,12 +1,54 @@
 <template>
-  <div class="stats-container default-page-margin">
+  <div
+    v-if="bootstrap" 
+    class="stats-container default-page-margin">
     <h1>Stats</h1>
+    <ul>
+      <StatsTeamListItem 
+        v-for="team in teams"
+        :key="team.id"
+        :team="team"
+        tag="li"
+      />
+    </ul>
   </div>
 </template>
 
 <script>
+import axios from "axios";
+import StatsTeamListItem from '@/components/stats/StatsTeamListItem.vue'
+
 export default {
-  name: 'Stats'
+  name: 'Stats',
+  components: {
+    StatsTeamListItem
+  },
+  data () {
+    return {
+      bootstrap: null,
+      teams: null,
+      BASE_URL: process.env.VUE_APP_FPL_API_URL,
+    }
+  },
+  mounted() {
+    this.getBootstrap();
+  },
+  methods: {
+    
+    getBootstrap() {
+      axios
+        .get(`${this.BASE_URL}/api/bootstrap-static/`)
+        .then((response) => {
+          this.bootstrap = response.data;
+          this.teams = response.data.teams
+          // this.allPlayers = response.data.elements;
+        })
+        .catch((error) => {
+          console.log(error);
+          // this.errored = true;
+        });
+    },
+  }
 }
 </script>
 
