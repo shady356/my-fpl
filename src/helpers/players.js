5a5592850147831c0c6c7733c1f17781d579049c
shady356 <heenriko@gmail.com>
2020-11-13 23:35:49 +0100

    feat: players helper methods

---
+++ src/helpers/players.js
@@ -0,0 +1,17 @@
+import store from '@/store/index.js'
+
+export const $getPlayerById = (playerId) => {
+  playerId = parseInt(playerId)
+  const allPlayers = store.state.bootstrap.elements
+  return allPlayers.find(player => {
+    return player.id === playerId
+  })
+}
+export const $getPlayerPosition = (position) => {
+  switch (position) {
+    case 1: return 'goalkeeper'
+    case 2: return 'defense';
+    case 3: return 'midfielder';
+    case 4: return 'forward';
+  }
+}
